08971ca37672277012e01ce8f010d760226d5b73
qvo117 <54693111+qvo117@users.noreply.github.com>
2021-10-03 20:35:19 +1300

    Email Subject improved and more comments added

--- sendMail.php
+++ sendMail.php
@@ -7,22 +7,32 @@ require_once "vendor/autoload.php";
 //Recieve POST from JS file 
 $email = $_POST['email'];
 $template = $_POST['template'];
+$subjectText = $_POST['subjectText'];
 
 //
 $mail = new PHPMailer(true);
 
-//Simple Mail Transfer Protocol
-$mail->SMTPDebug = 3;                               
-$mail->isSMTP();            
+//Simple Mail Transfer Protocol (SMTP)
+//Enable SMTP debugging
+$mail->SMTPDebug = 3; 
+
+//Set PHPMailer to use SMTP                    
+$mail->isSMTP();   
+
+//SMTP Hostname         
 $mail->Host = "smtp.gmail.com";
+
+//SMTP host requires authentication
 $mail->SMTPAuth = true;                          
 
 //Username and Password for email account
 $mail->Username = "maildistro13@gmail.com";                 
 $mail->Password = "Team13Mail";  
 
-//???
-$mail->SMTPSecure = "tls";                           
+//TLS encryption
+$mail->SMTPSecure = "tls";       
+
+//TCP Port
 $mail->Port = 587;                                   
 
 //Email Header
@@ -32,11 +42,11 @@ $mail->FromName = "Marks & Feedback";
 //Recipient Email
 $mail->addAddress("$email@aucklanduni.ac.nz","test");
 
-//set Email to not Html
+//set Email to not Html format
 $mail->isHTML(false);
 
 //Set Subject and Body for email
-$mail->Subject = "Subject Text";
+$mail->Subject = "$subjectText"; 
 $mail->Body = "$template";
 
 try {
