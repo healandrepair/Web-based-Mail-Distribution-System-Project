dcddeb71fb20922d96a5b1efa99033be5c0156ee
healandrepair <healandrepair@outlook.com>
2021-10-14 22:18:16 +1300

    changed IP address + table name

--- get.php
+++ get.php
@@ -3,11 +3,11 @@
 $username = 'root';
 $password = 'team13';
 $dbName = 'csvData';
-$dbHost = "34.129.166.167";
+$dbHost = "35.201.5.115";
 
 $lecturer =$_GET["lecturer"];
 $conn = mysqli_connect($dbHost, $username, $password,$dbName);
-$sql = "SELECT * FROM csvData.CsvData Where Lecturer = '$lecturer'" ;
+$sql = "SELECT * FROM csvData.CSVTable Where Lecturer = '$lecturer'" ;
 $result = mysqli_query($conn,$sql) or die("Error " . mysqli_error($conn));
 $results = mysqli_fetch_assoc($result);
 $resultstring = $results['CsvValues'];
--- post.php
+++ post.php
@@ -3,7 +3,7 @@
 $username = 'root';
 $password = 'team13';
 $dbName = 'csvData';
-$dbHost = "34.129.166.167";
+$dbHost = "35.201.5.115";
 
 $conn = mysqli_connect($dbHost, $username, $password,$dbName);
 if (isset($_POST['email'],$_POST['column'],$_POST['spreadSheet'],$_POST['csvValues'],$_POST['lecturer'])) {
@@ -12,7 +12,7 @@ if (isset($_POST['email'],$_POST['column'],$_POST['spreadSheet'],$_POST['csvValu
   $spreadSheet = $_POST['spreadSheet'];
   $csvValues = $_POST['csvValues'];
   $lecturer = $_POST['lecturer'];
-  $sql = "insert into CsvData values('$lecturer','$email','$column','$spreadSheet','$csvValues')";
+  $sql = "insert into csvData.CSVTable values('$lecturer','$email','$column','$spreadSheet','$csvValues')";
   $run = mysqli_query($conn,$sql) or die("Error " . mysqli_error($conn));
   echo "success";
 
