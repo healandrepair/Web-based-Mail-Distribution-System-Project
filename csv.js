89198e9f39f2714a23872c6150d71894568cd643
Markost2809 <68369093+Markost2809@users.noreply.github.com>
2021-10-20 17:43:56 +1300

    remove global variables

--- csv.js
+++ csv.js
@@ -1,23 +1,5 @@
-//User submitted email template
-var textTemplate = '';
-//CSV gathered columns
-var columns = [];
-//CSV gathered student data
-var csvValues = [];
-//Merged template and CSV data
+//Merged Emails
 var emails = {};
-//Name of CSV file
-var spreadSheet = '';
-//Name of lecturer
-var lecturer = '';
-//Subject Text
-var subjectText = '';
-//Database data text 
-var dbData = '';
-//Dict obj containing the student & their data
-var data = '';
-
-var storedCsv = '';
 
 //CSV & template elements from the form
 const myForm = document.getElementById("opener");
@@ -37,12 +19,17 @@ signOutbar = document.getElementById('signOutbar');
 
 /* Sends emails via a POST request */
 function sendMail() {
-    for (const [email, template] of Object.entries(emails)) {
+    for (var [student, email] of Object.entries(emails)) {
+        //get the subject text for the email and remove it from email template
+        var subjectLine = email.slice(email.indexOf('Subject'), email.indexOf('\n') + 1);
+        email = email.replace(subjectLine, '').trim();
+        var subjectText = subjectLine.slice(subjectLine.indexOf(':') + 1).trim();
+        console.log(email)
         //Create and Post AJAX HttpRequest to SendMail.php
         var httpr = new XMLHttpRequest();
         var fd = new FormData();
+        fd.append("student", student);
         fd.append("email", email);
-        fd.append("template", template);
         fd.append("subjectText", subjectText);
         httpr.onload = function() {
             const serverResponse = document.getElementById("serverResponse");
@@ -54,50 +41,62 @@ function sendMail() {
 
 /* Sends data to the database via a POST request */
 function sendData() {
-    for (let i = 0; i < csvValues.length; i++) {
-        for (const [column, value] of Object.entries(csvValues[i])) {
-            var httpr = new XMLHttpRequest();
-            var fd = new FormData();
-            fd.append("lecturer", lecturer);
-            fd.append("email", csvValues[i]["email"]);
-            fd.append("column", column);
-            fd.append("spreadSheet", spreadSheet);
-            fd.append("csvValues", value);
-            httpr.onload = function() {
-                //Testing purposes
-                const serverResponse = document.getElementById("serverResponse");
-                serverResponse.innerHTML = this.responseText;
+    const data = csvData(function(data,spreadSheet){
+        csvValues = data[1]
+        column = data[0]
+        lecturer = localStorage.getItem('lecturer')
+        for (let i = 0; i < csvValues.length; i++) {
+            for (const [column, value] of Object.entries(csvValues[i])) {
+                var httpr = new XMLHttpRequest();
+                var fd = new FormData();
+                fd.append("lecturer", lecturer);
+                fd.append("email", csvValues[i]["email"]);
+                fd.append("column", column);
+                fd.append("spreadSheet", spreadSheet);
+                fd.append("csvValues", value);
+                httpr.onload = function() {
+                    //Testing purposes
+                    const serverResponse = document.getElementById("serverResponse");
+                    serverResponse.innerHTML = this.responseText;
+                }
+                httpr.open("POST", "post.php");
+                httpr.send(fd);
             }
-            httpr.open("POST", "post.php");
-            httpr.send(fd);
         }
-    }
-    getStoredCsv()
+        getStoredCsv()
+    })
 }
+
 
 /* Retrieves data from the database via a GET request */
 async function getData() {
-    dbData=""
-    for (i in storedCsv) {
-        if (document.getElementById(storedCsv[i]).checked) {
-            var httpr = new XMLHttpRequest();
-            httpr.onload = function() {
-                const serverResponse = document.getElementById("serverResponse");
-                // serverResponse.innerHTML = this.responseText; 
-                dbData += this.responseText;
-                // calls function to convert data from database to obj
-                convertToObject()
-                mergeData()
-                showData()
+    var dbData=""
+    const storedCsv = getStoredCsvCallback(function(storedCsv){
+        for (i in storedCsv) {
+            console.log(document.getElementById(storedCsv[i]))
+            if (document.getElementById(storedCsv[i]).checked) {
+
+                var httpr = new XMLHttpRequest();
+                httpr.onload = function() {
+                    console.log("hi")
+                    const serverResponse = document.getElementById("serverResponse");
+                    // serverResponse.innerHTML = this.responseText; 
+                    dbData += this.responseText;
+                    // calls function to convert data from database to obj
+                    var data = convertToObject(dbData)
+                    mergeData(data)
+
+                }
+                httpr.open("GET", `get.php?lecturer=${lecturer}&spreadsheet=${storedCsv[i]}`);
+                httpr.send();
             }
-            httpr.open("GET", `get.php?lecturer=${lecturer}&spreadsheet=${storedCsv[i]}`);
-            httpr.send();
         }
-    }
+    })
+    
 }
 
 /* Converts retrieved data from database to object type.*/
-function convertToObject() {
+function convertToObject(dbData) {
     var arr = dbData.split("\n");
     var obj = {}
     for (i of arr) {
@@ -120,53 +119,66 @@ function convertToObject() {
         // console.log(i)
     }
     //console.log("obj", obj);
-    data = obj; //Stores the Object of Objects in data variable.
+    return obj; //Stores the Object of Objects in data variable.
 }
 async function csvMerge() {
     getData()
 }
 
+function getTemplate(callback){
+    input = txtFile.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var textTemplate = e.target.result;
+
+        textTemplate = textTemplate.replace(/</g, "&lt;").replace(/>/g, "&gt;"); // Stops potential html injection
+        callback(textTemplate)
+    };
+    reader.readAsText(input);
+}
+
+
 /* Merges retrieved data from database with email template.*/
-async function mergeData() {
-    emails = {};
-    //loop through each dict obj containing CSV values of each student
-    for (const [StudentEmail, StudentData] of Object.entries(data)) {
-        console.log(data)
-        var email = textTemplate;
-        //get the subject text for the email and remove it from email template
-        var subjectLine = email.slice(email.indexOf('Subject'), email.indexOf('\n') + 1);
-        email = email.replace(subjectLine, '').trim();
-        subjectText = subjectLine.slice(subjectLine.indexOf(':') + 1).trim();
-        while (email.indexOf('[') != -1) {
-            var placeholderStr = email.slice(email.indexOf('['), email.indexOf(']') + 1);
-            var header = email.slice(email.indexOf('[') + 1, email.indexOf(']'));
-            //If the header string exists in the dict obj, then replace it with the actual data
-            if (StudentData[header]) {
-                email = email.replace(placeholderStr, StudentData[header]);
-            } else {
-                //If the header string contains the if condition
-                if (header.includes('ifNonNull')) {
-                    //get the column to check the condition
-                    header = header.slice(header.indexOf(':') + 1).trim();
-                    //check whether the value is 0
-                    value = StudentData[header];
-                    if (value == '0') {
-                        //remove all text that this condition applies to
-                        var textToRemove = email.slice(email.indexOf('[ifNonNull'), email.indexOf('[endif]') + 7);
-                        email = email.replace(textToRemove, '');
-                    } else {
-                        email = email.replace(placeholderStr, '');
-                        email = email.replace('[endif]', '');
+async function mergeData(data) {
+    const template = getTemplate(function(textTemplate){
+        emails = {};
+        //loop through each dict obj containing CSV values of each student
+        for (const [StudentEmail, StudentData] of Object.entries(data)) {
+            console.log(email)
+            var email = textTemplate;
+
+            while (email.indexOf('[') != -1) {
+                var placeholderStr = email.slice(email.indexOf('['), email.indexOf(']') + 1);
+                var header = email.slice(email.indexOf('[') + 1, email.indexOf(']'));
+                //If the header string exists in the dict obj, then replace it with the actual data
+                if (StudentData[header]) {
+                    email = email.replace(placeholderStr, StudentData[header]);
+                } else {
+                    //If the header string contains the if condition
+                    if (header.includes('ifNonNull')) {
+                        //get the column to check the condition
+                        header = header.slice(header.indexOf(':') + 1).trim();
+                        //check whether the value is 0
+                        value = StudentData[header];
+                        if (value == '0') {
+                            //remove all text that this condition applies to
+                            var textToRemove = email.slice(email.indexOf('[ifNonNull'), email.indexOf('[endif]') + 7);
+                            email = email.replace(textToRemove, '');
+                        } else {
+                            email = email.replace(placeholderStr, '');
+                            email = email.replace('[endif]', '');
+                        }
                     }
                 }
             }
+            //adds newline chars at the end of each student's email content
+            email += '\n\n------------\n\n'
+            emails[StudentEmail] = email
         }
-        //adds newline chars at the end of each student's email content
-        email += '\n\n------------\n\n'
-        emails[StudentEmail] = email
-    }
-
-    console.log("emails", emails);
+    
+        console.log("emails", emails);
+        showData()
+    })
 
 
 
@@ -453,20 +465,15 @@ function sortTable(n) {
 }
 
 /* Read the CSV file & create the CSV table */
-myForm.addEventListener("submit", function(e) {
-    e.preventDefault();
+function previewData() {
     const input = csvFile.files[0];
     const reader = new FileReader();
-    spreadSheet = csvFile.value.substring(csvFile.value.lastIndexOf("\\") + 1);
-
+    const spreadSheet = csvFile.value.substring(csvFile.value.lastIndexOf("\\") + 1);
     reader.onload = function(e) {
         const text = e.target.result;
         const data = csvToArray(text);
         const headers = data[0]
         const values = data[1]
-        columns = data[0];
-        csvValues = data[1];
-        console.log("csvalues", csvValues);
 
         // Create table
         let main = document.getElementById("tableDiv");
@@ -517,13 +524,78 @@ myForm.addEventListener("submit", function(e) {
         main.appendChild(table);
     };
     reader.readAsText(input);
-})
+
+}
+
+function csvData(callback) {
+    const input = csvFile.files[0];
+    const reader = new FileReader();
+    const spreadSheet = csvFile.value.substring(csvFile.value.lastIndexOf("\\") + 1);
+    reader.onload = function(e) {
+        const text = e.target.result;
+        const data = csvToArray(text);
+        const headers = data[0]
+        const values = data[1]
+
+        // Create table
+        let main = document.getElementById("tableDiv");
+        let table = document.createElement('table');
+        table.setAttribute("id", "dataTable");
+
+        // Inserts row for headers
+        table.insertRow();
+
+        // Adds header values to header row
+        for (h of headers) {
+            let hcell = table.rows[table.rows.length - 1].insertCell();
+            hcell.textContent = h;
+        }
+
+        // Adds array values corresponding to the correct header to table
+        for (arr of values) {
+            table.insertRow();
+            for (let i = 0; i < headers.length; i++) {
+                let vcell = table.rows[table.rows.length - 1].insertCell();
+                vcell.textContent = arr[headers[i]];
+            }
+        }
+
+        // Gets the email header cell
+        let email = table.rows[0].cells[0];
+        // Gets the PrefName header cell
+        let name = table.rows[0].cells[1];
+
+        // Adds ability to sort by header cell email with a mouse click
+        email.setAttribute('onclick', 'sortTable(0);');
+        // Adds pointer cursor on hover
+        email.setAttribute('style', 'cursor: pointer;');
+        // Sets class to invert colors on highlight
+        email.setAttribute('class', 'cellh');
+
+        // Adds ability to sort by header cell name with a mouse click
+        name.setAttribute('onclick', 'sortTable(1);');
+        // Adds pointer cursor on hover
+        name.setAttribute('style', 'cursor: pointer;');
+        // Sets class to invert colors on highlight
+        name.setAttribute('class', 'cellh');
+
+        // Clears table div to show the current file
+        main.innerHTML = "";
+
+        // Adds table to page
+        main.appendChild(table);
+        callback(data,spreadSheet)
+    };
+    reader.readAsText(input);
+
+}
 
 /* Login stuff */
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
     lecturer = profile.getGivenName();
+    localStorage.setItem('lecturer',lecturer)
     //get all stored CSV files of the lecturer
     getStoredCsv()
     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
@@ -562,32 +634,61 @@ function signOut() {
 };
 
 /* Gets all the stored CSV files of the lecturer */
-function getStoredCsv() {
-    document.getElementById("showfiles").innerHTML = "";
+function getStoredCsvCallback(callback) {
     var httpr = new XMLHttpRequest();
     httpr.onload = function() {
         const serverResponse = document.getElementById("serverResponse");
-        storedCsv = this.responseText.split(',')
-        console.log("storedCsv", storedCsv);
+        var storedCsv = this.responseText.split(',')
         // serverResponse.innerHTML = this.responseText; // Right not it prints the data out at @serviceResponse
         for (i in this.responseText.split(',')) {
-            console.log(i)
-            var myDiv = document.getElementById("showfiles");
+            if (document.getElementById(this.responseText.split(',')[i]) == null){
+                var myDiv = document.getElementById("showfiles");
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.name = "name";
+                checkbox.value = "value";
+                checkbox.id = this.responseText.split(',')[i];
+    
+                var label = document.createElement('label');
+    
+                label.htmlFor = this.responseText.split(',')[i];
+    
+                label.appendChild(document.createTextNode(this.responseText.split(',')[i]));
+                myDiv.appendChild(checkbox);
+                myDiv.appendChild(label);
+            }
 
-            var checkbox = document.createElement('input');
+        }
+        callback(storedCsv)
+    }
+    httpr.open("GET", `getcsv.php?lecturer=${lecturer}`);
+    httpr.send();
+}
 
-            checkbox.type = "checkbox";
-            checkbox.name = "name";
-            checkbox.value = "value";
-            checkbox.id = this.responseText.split(',')[i];
+function getStoredCsv() {
+    var httpr = new XMLHttpRequest();
+    httpr.onload = function() {
+        const serverResponse = document.getElementById("serverResponse");
+        var storedCsv = this.responseText.split(',')
+        // serverResponse.innerHTML = this.responseText; // Right not it prints the data out at @serviceResponse
+        for (i in this.responseText.split(',')) {
+            if (document.getElementById(this.responseText.split(',')[i]) == null){
+                var myDiv = document.getElementById("showfiles");
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.name = "name";
+                checkbox.value = "value";
+                checkbox.id = this.responseText.split(',')[i];
+    
+                var label = document.createElement('label');
+    
+                label.htmlFor = this.responseText.split(',')[i];
+    
+                label.appendChild(document.createTextNode(this.responseText.split(',')[i]));
+                myDiv.appendChild(checkbox);
+                myDiv.appendChild(label);
+            }
 
-            var label = document.createElement('label');
-
-            label.htmlFor = this.responseText.split(',')[i];
-
-            label.appendChild(document.createTextNode(this.responseText.split(',')[i]));
-            myDiv.appendChild(checkbox);
-            myDiv.appendChild(label);
         }
     }
     httpr.open("GET", `getcsv.php?lecturer=${lecturer}`);
